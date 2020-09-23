eac48f784e86e0f32f7a5b6bfe62fa048c0d6bf1
oleksandravozniuk <16alexandra09@gmail.com>
2020-09-24 02:11:09 +0300

    done lab2

--- SystemsModelling2/SystemsModelling2/Model.cs
+++ SystemsModelling2/SystemsModelling2/Model.cs
@@ -7,13 +7,17 @@ namespace SystemsModelling2
 {
     public class Model
     {
-        private double tnext;
-        private double tcurr;
+        private double tnext;//наступний момент часу
+        private double tcurr;//поточний момент часу
         private double t0, t1;
         private double delayCreate, delayProcess;
-        private int numCreate, numProcess, failure;
+        private int numCreate; // кількість створених запитів
+        private int numProcess; 
+        private int failure; //кількість відмов
         private int state, maxqueue, queue;
-        private int nextEvent;
+        private int nextEvent;//чи є на обробці події
+
+        private double deltaT;
 
         private ExpDistribution expDistribution = new ExpDistribution(); 
 
@@ -35,12 +39,20 @@ namespace SystemsModelling2
             tnext = 0.0;
             nextEvent = 0;
             tcurr = tnext;
-            t0 = tcurr; t1 = Double.MaxValue;
+            t0 = tcurr; 
+            t1 = Double.MaxValue;
             maxqueue = maxQ;
         }
 
         public void Simulate(double timeModelling)
         {
+            List<double> tServices = new List<double>();
+            int i = 0;
+
+            double tServiceSum = 0;
+
+
+
             while(tcurr<timeModelling)
             {
                 tnext = t0;
@@ -51,42 +63,58 @@ namespace SystemsModelling2
                     tnext = t1;
                     nextEvent = 1;
                 }
+
+                deltaT = deltaT + ((tnext - tcurr) * state);
+
                 tcurr = tnext;
+
+                if(nextEvent==0)
+                {
+                    tServices.Add(tcurr);
+                }
 
                 switch (nextEvent)
                 {
                     case 0: event0(); break;
-                    case 1: event1();break;
+                    case 1: event1();tServiceSum = tServiceSum + (tServices[i] = tcurr - tServices[i]);i++; break;
                 }
-                printInfo();
+
+               // printInfo();
+                
             }
-            printStatistic();
+            double rAver = deltaT / tnext;//середнє завантаження пристрою
+            double tNet = tServiceSum/numProcess;//середній час очікування
+            double failureProbability = (double)failure /(double) numCreate;//вірогідність відмови
+
+            Console.WriteLine(delayCreate + "            " + delayProcess + "            " + maxqueue + "          " + rAver + "                " + tNet + "        " + failureProbability);
+
+            //printStatistic();
         }
 
-        public void event0()
+        public void event0()//подія надходження
         {
-            t0 = tcurr + getDelayCreate();
-            numCreate++;
+            t0 = tcurr + getDelayCreate();//момент часу надходження наступної події на обслуговування
+            numCreate++;//збільшення кількості елементі, які вже обслугувались
             if (state == 0)
             {
-                state = 1;
-                t1 = tcurr + getDelayProcess();
+                state = 1;// занято
+                t1 = tcurr + getDelayProcess(); //момент часу закінчення обслуговування
             }
             else
             {
-                if (queue < maxqueue) queue++;
-                else failure++;
+                if (queue < maxqueue) queue++;//додати елемент до черги
+                else failure++;//додати елемент до числа відмов
             }
         }
-        public void event1()
+        public void event1()//подія закінчення обслуговування на каналі
         {
             t1 = Double.MaxValue;
-            state = 0;
-            if (queue > 0)
+            state = 0;//канал вільний
+            if (queue > 0)//якщо в черзі є елементи
             {
-                queue--;
-                state = 1;
-                t1 = tcurr + getDelayProcess();
+                queue--;//зменшити чергу
+                state = 1;//перевести у стан занято канал
+                t1 = tcurr + getDelayProcess();//момент часу закінчення обслуговування
             }
             numProcess++;
         }
@@ -98,7 +126,11 @@ namespace SystemsModelling2
         }
         public void printInfo()
         {
-            Console.WriteLine(" t= " + tcurr + " state = " + state + " queue = " + queue);
+            Console.WriteLine(" t= " + tcurr + " state = " + state + " queue = " + queue );
+        }
+        public void printInfo2()
+        {
+            Console.WriteLine("tcurr = " + tcurr + " tnext = " + tnext + " t0 = " + t0 + " t1 = " + t1 + "\n");
         }
         private double getDelayCreate()
         {
--- SystemsModelling2/SystemsModelling2/Program.cs
+++ SystemsModelling2/SystemsModelling2/Program.cs
@@ -6,9 +6,36 @@ namespace SystemsModelling2
     {
         static void Main(string[] args)
         {
-            Model model = new Model(2, 1, 5);
-            model.Simulate(1000);
+            Console.WriteLine("DelayCreate " + "DelayProcess " + "MaxQueue " + "   R average " + "                    T net "+ "                  FailureProbability");
+            for(int i=0;i<10;i++)
+            {
+                Model model = new Model(2, 1, 5);
+                model.Simulate(1000);
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("DelayCreate " + "DelayProcess " + "MaxQueue " + "   R average " + "                    T net " + "                  FailureProbability");
+            for (int i = 0; i < 10; i++)
+            {
+                Model model = new Model(2, 1, i+1);
+                model.Simulate(1000);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("DelayCreate " + "DelayProcess " + "MaxQueue " + "      R average " + "                        T net " + "                  FailureProbability");
+            for (int i = 0; i < 10; i++)
+            {
+                Model model = new Model(2, i+1, 5);
+                model.Simulate(1000);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("DelayCreate " + "DelayProcess " + "MaxQueue " + "      R average " + "                        T net " + "                  FailureProbability");
+            for (int i = 0; i < 10; i++)
+            {
+                Model model = new Model(i+1, 1, 5);
+                model.Simulate(1000);
+            }
         }
     }
 }
